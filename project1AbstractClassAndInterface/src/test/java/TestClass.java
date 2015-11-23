73bf092803b9eecb36c82e6c78e3e889b66386b2
BarannikovIS <barannikov.vanya.95@yandex.ru>
2015-11-24 00:58:11 +0300

    + немного Mockito

--- project1AbstractClassAndInterface/src/test/java/TestClass.java
+++ project1AbstractClassAndInterface/src/test/java/TestClass.java
@@ -3,6 +3,7 @@ import com.mycompany.project1abstractclassandinterface.Rectangle;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
+import static org.mockito.Mockito.*;
 
 /**
  *
@@ -13,21 +14,27 @@ public class TestClass {
     Figure r;
     
     @Before
-    public void init(){    
-        r= new Rectangle(1,2,15,20);  
+    public void init(){ 
+        r= spy(new Rectangle(1,2,15,20));
     }
     @Test
     public void moveTest(){
         r.move(2, 2);
+        
+        when(r.getX()).thenReturn(3);
+        when(r.getY()).thenReturn(4);
+        
         assertEquals(r.getX(),3);
         assertEquals(r.getY(),4);
     }
     @Test
     public void perimetrTest(){
+        when(r.calculateThePerimetr()).thenReturn(70);
         assertEquals(r.calculateThePerimetr(),70);
     }
     @Test
     public void areaTest(){
+        when(r.calculateTheArea()).thenReturn(300);
         assertEquals(r.calculateTheArea(),300);
     }
 }
